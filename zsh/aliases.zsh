aaa94f4f3a1f2403bbfd952b0d771222743b9220
Kevin Suttle <kevin@kevinsuttle.com>
2012-09-10 22:16:24 -0400

    More consistent, and stupid useful aliases
    Thanks to @mathiasbynens and @holman for
    the awesome shortcuts. Most of these are mine
    though. :)
    
    Most of these are for git, but some of them are
    for what I'm calling QOD, or 'Quality of Development';
    making day-to-day work easier.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,20 @@
 # UNIX
-alias ls='ls -laHF'
+alias ls='ls -aFh'
+alias lso='ls -oaFh'
+
+# Easier navigation: .., ..., ...., ....., ~ and -
+# Thanks @mathias, et al.
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ~'cd ~'
+alias -- -='cd -'
 alias cddf='cd ~/Github/dotfiles'
+
+# Shortcuts
+alias v='vim'
+alias s='subl .'
+alias o='open -R'
+alias t='tmux'
+alias rz='source $HOME/.zshrc'
