fdd86c7a63af3dfa28ca501b6159022a93c0f4a0
Kevin Suttle <kevin@kevinsuttle.com>
2012-09-10 23:52:35 -0400

    Add hub
    "Use `hub` as our git wrapper:
    http://defunkt.github.com/hub/"
    
    - Thanks @holman

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,4 +1,11 @@
-# Git aliases
+# Use `hub` as our git wrapper:
+# http://defunkt.github.com/hub/
+hub_path=$(which hub)
+if [[ -f $hub_path ]]
+then
+	alias git=$hub_path
+fi
+
 alias gs='git status -sb'
 alias gp='git push'
 alias gpom='git push origin master'
