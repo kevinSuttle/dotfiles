9ef20fab23194bd86ec5683a4b827c7fcd625557
Kevin Suttle <kevin@kevinsuttle.com>
2013-02-01 15:38:08 -0500

    Remove unused hub command

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,12 +1,3 @@
-# Use `hub` as our git wrapper:
-# http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-        alias git=$hub_path
-fi
-
-# Git aliases
 alias gs='git status -sb'
 alias gph='git push'
 alias gpl='git pull'
