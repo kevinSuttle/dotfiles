49ce90d3e2766409a4ef70b4965b4976cf341e86
Kevin Suttle <kevin@kevinsuttle.com>
2013-02-12 23:16:10 -0500

    Clean up zsh shell aliases
    Get rid of unused
    Add more frequently used

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,20 +1,14 @@
 # UNIX
 alias ls='ls -aFh'
-alias lso='ls -oaFh'
 
-# Easier navigation: .., ..., ...., ....., ~ and -
-# Thanks @mathias, et al.
+# Easier navigation: Thanks @mathias, @gf3, et al.
 alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias -- -='cd -'
 alias cddf='cd ~/Github/dotfiles'
+alias cdg='cd ~/Github/'
+alias cdd='cd ~/Downloads/'
 
 # Shortcuts
-alias v='mvim -v'
-alias s='subl .'
-alias o='open -R'
-alias t='tmux'
-alias rz='source $HOME/.zshrc'
-alias td='todo.sh -d ~/.todo.cfg'
+alias v='mvim -vp'
+
+# UPDATE ALL THE THINGS
+alias THUNDERCATS HO='brew update && gem update && npm update'
