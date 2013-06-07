eeaafa41ade3cb2de755f9e26987a03809eac859
Kevin Suttle <kevin@kevinsuttle.com>
2013-06-07 02:36:23 -0400

    Update Ruby when updating system
    Also bump versions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,5 +17,5 @@ alias mvim='~/Applications/MacVim.app/Contents/MacOS/Vim -vf'
 
 # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
 # Thanks once again @Mathias
-alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update'
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update; cd ~/.rbenv/ && git pull; cd ~/.rbenv/plugins/ruby-build/ && git pull; cd'
 
