e4630072c66e5a3fffe4b35c1e18f95b66734f75
Kevin Suttle <kevin@kevinsuttle.com>
2013-08-16 18:29:15 -0400

    Seriously, how hard are 2 dashes?

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,7 +18,7 @@ alias mvim='~/Applications/MacVim.app/Contents/MacOS/Vim -vf'
 
 # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
 # Thanks once again @Mathias
-alias update='sudo softwareupdate -i -a; brew update && brew upgrade brew-cask; brew upgrade; brew tap --repair; brew prune; brew cleanup; brew doctor; npm update npm -g; npm update -g; sudo gem update; cd ~/.rbenv/ && git pull -—rebase; cd ~/.rbenv/plugins/ruby-build/ && git pull -—rebase; cd'
+alias update='sudo softwareupdate -i -a; brew update && brew upgrade brew-cask; brew upgrade; brew tap --repair; brew prune; brew cleanup; brew doctor; npm update npm -g; npm update -g; sudo gem update; cd ~/.rbenv/ && git pull --rebase; cd ~/.rbenv/plugins/ruby-build/ && git pull --rebase; cd'
 
 # Commands
 alias jsw='jekyll serve --watch'
