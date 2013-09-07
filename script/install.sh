cb34ddb1515860fb11e51feb6e62168e47632205
Kevin Suttle <kevin@kevinsuttle.com>
2013-09-07 15:58:56 -0400

    Refactor bootstrap installers
    Thanks again @thoughbot.

---
+++ script/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+for name in *; do
+  target="$HOME/.$name"
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      echo "WARNING: $target exists but is not a symlink."
+    fi
+  else
+    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
+      echo "Creating $target"
+      ln -s "$PWD/$name" "$target"
+    fi
+  fi
+done
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim -u ~/.vimrc.bundles +BundleInstall +qa
