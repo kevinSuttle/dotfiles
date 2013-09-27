36cff9c3eae011395b032f55b73d9e1cecb9d44b
Kevin Suttle <kevin@kevinsuttle.com>
2013-09-27 11:57:30 -0400

    Don't remove files that the install script didn't add

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
 
 rm $HOME/.bash*
-rm $HOME/.curlrc
-rm $HOME/.gemrc
-rm $HOME/.git*
-rm $HOME/.gvim*
-rm $HOME/.hg*
-rm $HOME/.hushlogin
-rm $HOME/.inputrc 
-rm $HOME/.netrc
-rm $HOME/.screenrc
-rm $HOME/.tmux*
-rm $HOME/.z*
-rm -d $HOME/.vim*
