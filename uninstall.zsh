375aae920403626c76f84d9fec2eaa56b507cd62
Kevin Suttle <kevin@suttle.io>
2013-12-04 22:20:38 -0500

    Clean up uninstall script

--- uninstall.zsh
+++ uninstall.zsh
@@ -1,13 +1,4 @@
-rm $HOME/.bashrc
-rm $HOME/.curlrc
-rm $HOME/.rbenv/default-gems
-rm $HOME/.gemrc
+rm $HOME/.bash*
 rm $HOME/.git*
-rm $HOME/.gvim*
-rm $HOME/.hg*
 rm $HOME/.hushlogin
-rm $HOME/.inputrc
-rm $HOME/.screenrc
-rm $HOME/.tmux*
-rm -dRf $HOME/.vim*
-rm $HOME/.zshrc
+rm $HOME/.z*
