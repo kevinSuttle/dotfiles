e139551808f74e91b71172067487d34f81bdbc8b
Kevin Suttle <kevin@suttle.io>
2013-12-08 23:42:59 -0500

    Add existence check for dotfile repo before cloning again

--- install.zsh
+++ install.zsh
@@ -1,10 +1,16 @@
-git clone https://github.com/kevinSuttle/dotfiles.git ~/Code/dotfiles
-export DOTFILES="$HOME/Code/dotfiles"
-
-ln -sf $DOTFILES/bashrc $HOME/.bashrc
-ln -sf $DOTFILES/gitconfig $HOME/.gitconfig
-ln -sf $DOTFILES/gitignore $HOME/.gitignore
-ln -sf $DOTFILES/hushlogin $HOME/.hushlogin
-ln -sf $DOTFILES/zshrc $HOME/.zshrc
+# Don't try to clone the dotfile repo again if it already exists
+if [[ -d ~/Code/dotfiles ]]
+then
+	ln -sf $DOTFILES/bashrc $HOME/.bashrc
+	ln -sf $DOTFILES/gitconfig $HOME/.gitconfig
+	ln -sf $DOTFILES/gitignore $HOME/.gitignore
+	ln -sf $DOTFILES/hushlogin $HOME/.hushlogin
+	ln -sf $DOTFILES/npmrc $HOME/.npmrc
+	ln -sf $DOTFILES/zshrc $HOME/.zshrc
+else
+	git clone https://github.com/kevinSuttle/dotfiles.git ~/Code/dotfiles
+    export DOTFILES="$HOME/Code/dotfiles"
+    git clone https://github.com/sindresorhus/pure ~/Code/pure/
+fi
 
 exec $SHELL -l
