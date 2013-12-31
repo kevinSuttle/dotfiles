2cb930bff7dfcc07f668ee38974bc714be8b0583
Kevin Suttle <kevin@suttle.io>
2013-12-31 14:46:01 -0500

    Remove references to dotfiles $PATH
    Tripping things up elsewhere. Just set it locally.

--- install.zsh
+++ install.zsh
@@ -1,16 +1,16 @@
 # Don't try to clone the dotfile repo again if it already exists
 if [[ -d ~/Code/dotfiles ]]
 then
+  export DOTFILES="$HOME/Code/dotfiles"
 	ln -sf $DOTFILES/bashrc $HOME/.bashrc
 	ln -sf $DOTFILES/gitconfig $HOME/.gitconfig
 	ln -sf $DOTFILES/gitignore $HOME/.gitignore
-	ln -sf $DOTFILES/hushlogin $HOME/.hushlogin
 	ln -sf $DOTFILES/npmrc $HOME/.npmrc
+	ln -sf $DOTFILES/hushlogin $HOME/.hushlogin
 	ln -sf $DOTFILES/zshrc $HOME/.zshrc
 else
 	git clone https://github.com/kevinSuttle/dotfiles.git ~/Code/dotfiles
-    export DOTFILES="$HOME/Code/dotfiles"
-    git clone https://github.com/sindresorhus/pure ~/Code/pure/
+  git clone https://github.com/sindresorhus/pure ~/Code/pure-zsh/
 fi
 
 exec $SHELL -l
