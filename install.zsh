6698cdced97fdd5fb20101922f33caf8fd8857eb
Kevin Suttle <kevin@suttle.io>
2014-01-01 15:27:07 -0500

    Add static output to install/uninstall scripts
    WIP: Dynamic output to come with conditional logic

--- install.zsh
+++ install.zsh
@@ -7,10 +7,15 @@ then
   ln -sf $HOME/Code/dotfiles/zlogin $HOME/.zlogin
   ln -sf $HOME/Code/dotfiles/zshenv $HOME/.zshenv
 	ln -sf $HOME/Code/dotfiles/zshrc $HOME/.zshrc
+  echo "Dotfiles have been symlinked to $HOME."
 else
+  echo "Cloning your dotfiles from GitHub..."
 	git clone https://github.com/kevinSuttle/dotfiles.git $HOME/Code/dotfiles
+  echo "Cloning Pure Zshell config from GitHub..."
   git clone https://github.com/sindresorhus/pure $HOME/Code/pure-zsh/
+  echo "Linking Pure Zshell prompt to Zsh functions..."
   ln -s "$HOME/Code/pure-zsh/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
+  echo "Dotfiles install complete."
 fi
 
 exec $SHELL -l
--- uninstall.zsh
+++ uninstall.zsh
@@ -1,3 +1,4 @@
 rm $HOME/.git*
 rm $HOME/.hushlogin
 rm $HOME/.z*
+echo "Your dotfiles have been removed from $HOME."
