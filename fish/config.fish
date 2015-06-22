2582b56fc894ca007166ab2fa666ab2909b5294e
kevinSuttle <--local>
2015-06-22 11:10:43 -0500

    Set boot2docker env. vars dynamically

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,9 @@ set -x MANPAGER "less -X"
 set -x GIT_TERMINAL_PROMPT 1
 set -x GOPATH $HOME/go
 set -x DOTFILES $HOME/Code/dotfiles
+set -x DOCKER_HOST tcp://(boot2docker ip):2376
+set -x DOCKER_CERT_PATH /Users/(whoami)/.boot2docker/certs/boot2docker-vm
+set -x DOCKER_TLS_VERIFY 1
 
 # Path
 if not set -q -U fish_user_paths
