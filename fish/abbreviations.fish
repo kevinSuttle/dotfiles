27e02609263107ca8c4fc4d26b466aaa2e682d88
kevinSuttle <--local>
2015-06-22 11:20:24 -0500

    Add fish config and abbreviations

---
+++ fish/abbreviations.fish
@@ -0,0 +1,50 @@
+abbr -a ga git add
+abbr -a gb git branch --verbose
+abbr -a gc git commit -s --verbose
+abbr -a gcl git clone --single-branch --verbose
+abbr -a gco git checkout
+abbr -a gcom git checkout master
+abbr -a gd git diff
+abbr -a gf git fetch --prune --all
+abbr -a gh git help
+abbr -a gl 'git log --graph --pretty format:"%C(cyan)%h%C(red)%d %C(yellow)%ar%C(white) %s %C(black)- %an"'
+abbr -a gm git merge --no-ff
+abbr -a gph git push
+abbr -a gpl git pull  --prune
+abbr -a gplr git pull --rebase
+abbr -a gr git remote --verbose
+abbr -a gs git status --short --branch
+abbr -a gt git tag
+abbr -a gu "git reset --soft 'HEAD^'"
+
+# List files, including hidden ones
+abbr -a ls ls -1pAFG
+# TODO make this a function
+abbr -a shf defaults write com.apple.finder AppleShowAllFiles -bool true
+abbr -a hhf defaults write com.apple.finder AppleShowAllFiles -bool false
+
+# Easier navigation: Thanks @mathiasbynens, @gf3, et al.
+abbr -a .. cd ..
+abbr -a cdc cd ~/Code/
+abbr -a cdd cd ~/Downloads
+abbr -a cddf cd ~/Code/dotfiles
+abbr -a cdl cd ~/Code/laptop
+abbr -a cds cd ~/Code/kevinsuttle.github.io
+abbr -a cdb cd ~/Code/bluemix-beta
+
+# Application Shortcuts
+abbr -a st open -a "Sublime Text"
+abbr -a start_server "python -m SimpleHTTPServer"
+
+# Commands
+abbr -a get_sha "openssl dgst -sha256 <"
+abbr -a serve python -m SimpleHTTPServer
+abbr -a copyssh "pbcopy < ~/.ssh/id_rsa.pub"
+abbr -a update_brew brew update; brew upgrade; brew cleanup; brew prune; brew doctor;
+
+abbr -a efc atom ~/.config/fish/config.fish
+abbr -a eff atom ~/.config/fish/functions
+abbr -a efp atom ~/.config/fish/functions/fish_prompt
+abbr -a efa atom ~/.config/fish/abbreviations.fish
+abbr -a r source ~/.config/fish/config.fish
+abbr -a f "exec '/Applications/fish.app/Contents/Resources/base/bin/fish'"
---
+++ fish/config.fish
@@ -0,0 +1,26 @@
+if not status --is-interactive
+  exit 0
+end
+
+source "$HOME/.config/fish/abbreviations.fish"
+
+# Fishhh
+set -x FISH_GREETING ""
+
+# Editor prefs
+set -x EDITOR vim
+
+# Don’t clear the screen after quitting a manual page
+set -x MANPAGER "less -X"
+
+set -x GIT_TERMINAL_PROMPT 1
+set -x GOPATH $HOME/go
+set -x DOTFILES $HOME/Code/dotfiles
+set -x DOCKER_HOST tcp://(boot2docker ip):2376
+set -x DOCKER_CERT_PATH /Users/(whoami)/.boot2docker/certs/boot2docker-vm
+set -x DOCKER_TLS_VERIFY 1
+
+# Path
+if not set -q -U fish_user_paths
+  set -U fish_user_paths "/usr/bin" "/usr/local/bin" "$GOPATH/bin"
+end
