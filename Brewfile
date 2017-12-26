6f6a7dec5ab490b4eb348ea2931b2320a9633d7f
Kevin Suttle <kevin@suttle.email>
2017-12-26 14:03:42 -0500

    Update config, add Brewfile, setup files

---
+++ Brewfile
@@ -0,0 +1,33 @@
+cask_args appdir: "~/Applications"
+
+brew "git"
+brew "readline"
+brew "rbenv"
+brew "ruby-build"
+brew "nvm"
+brew "yarn", args: ["without-node"]
+brew "vim"
+brew "cask"
+brew "mas"
+
+cask "spideroakone"
+cask "firefox"
+cask "1password"
+cask "cleanmymac"
+cask "tower"
+cask "iterm2"
+cask "alfred"
+cask "visual-studio-code"
+cask "flux"
+cask "slack"
+cask "docker"
+cask "private-internet-access"
+cask "moom"
+cask "torbrowser"
+
+mas "GIPHY Capture", id: 668208984
+mas "bear", id: 1091189122
+mas "keynote", id: 409183694
+mas "pages", id: 409201541
+mas "ia-writer", id: 775737590
+mas "kindle", id: 405399194
---
+++ setup.sh
@@ -0,0 +1,39 @@
+#! env sh
+
+if ( brew --version ) < /dev/null > /dev/null 2>&1; then 
+    echo 'Homebrew is already installed!' 
+else
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; 
+fi
+
+if ( brew cask --version; ) < /dev/null > /dev/null 2>&1; then 
+    echo 'Caskroom tapped already' 
+else
+    brew tap caskroom/cask;
+fi
+
+if ( brew bundle check; ) < /dev/null > /dev/null 2>&1; then 
+    echo 'Brewfiles enabled' 
+else
+    brew tap Homebrew/bundle;
+    brew bundle;
+fi
+
+brew cleanup;
+brew prune;
+brew doctor;
+
+cd ~; 
+git init; 
+git clone https://github.com/kevinSuttle/dotfiles.git .;
+git checkout -t origin/master
+git remote add origin https://github.com/kevinSuttle/dotfiles.git;
+git fetch origin;
+mkdir -p ~/Code && cd $_;
+
+git clone https://github.com/kevinSuttle/macOS-Defaults.git macOS-defaults -b suttle && cd $_;
+source .osx;
+
+rbenv install 2.4.3;
+mkdir -p ~/.nvm;
+nvm install --lts;
