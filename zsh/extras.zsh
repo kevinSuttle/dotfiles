61b5a74acd407af08881b4633192bf0f4bc0779d
Kevin Suttle <kevin@suttle.email>
2018-04-06 10:13:17 -0400

    Add OCaml configs

--- zsh/extras.zsh
+++ zsh/extras.zsh
@@ -2,3 +2,6 @@ test -e ${HOME}/.iterm2_shell_integration.zsh && source ${HOME}/.iterm2_shell_in
 
 # brew info zsh-syntax-highlighting
 test -e /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh && source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# OPAM configuration
+. /Users/kevinsuttle/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
